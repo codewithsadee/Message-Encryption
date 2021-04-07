3a914c7bd40e38f8d0dc2db0404a9d1ec1fc0033
Sadee <mohammadsadee24@gmail.com>
2021-04-08 01:16:49 +0600

    Change the Encryption Engine to Better Algorithm. <><><> Credit: tokee24

--- script.js
+++ script.js
@@ -1,73 +1,73 @@
-var userInput, letterCollect, letters;
-userInput = prompt("Enter your password").toLowerCase();
-letterCollect = "";
-for (x = 0; x < userInput.length; x++) {
-  letters = userInput.charAt(x);
-  if (letters == "a") {
-    letterCollect += "01";
-  } else if (letters == "b") {
-    letterCollect += "02";
-  } else if (letters == "c") {
-    letterCollect += "03";
-  }
-  if (letters == "d") {
-    letterCollect += "04";
-  } else if (letters == "e") {
-    letterCollect += "05";
-  } else if (letters == "f") {
-    letterCollect += "06";
-  }
-  if (letters == "g") {
-    letterCollect += "07";
-  } else if (letters == "h") {
-    letterCollect += "08";
-  } else if (letters == "i") {
-    letterCollect += "09";
-  }
-  if (letters == "j") {
-    letterCollect += "10";
-  } else if (letters == "k") {
-    letterCollect += "11";
-  } else if (letters == "l") {
-    letterCollect += "12";
-  }
-  if (letters == "m") {
-    letterCollect += "13";
-  } else if (letters == "n") {
-    letterCollect += "14";
-  } else if (letters == "o") {
-    letterCollect += "15";
-  }
-  if (letters == "p") {
-    letterCollect += "16";
-  } else if (letters == "q") {
-    letterCollect += "17";
-  } else if (letters == "r") {
-    letterCollect += "18";
-  }
-  if (letters == "s") {
-    letterCollect += "19";
-  } else if (letters == "t") {
-    letterCollect += "20";
-  } else if (letters == "u") {
-    letterCollect += "21";
-  }
-  if (letters == "v") {
-    letterCollect += "22";
-  } else if (letters == "w") {
-    letterCollect += "23";
-  } else if (letters == "x") {
-    letterCollect += "24";
-  }
-  if (letters == "y") {
-    letterCollect += "25";
-  } else if (letters == "z") {
-    letterCollect += "26";
+/*
+  ===<>===<>===<>===<>===<>===<>===
+  ===<>===Encryption Engine===<>===
+  ===<>===<>===<>===<>===<>===<>===
+*/
+
+// Declare All variables
+var userInput,
+  letter,
+  number,
+  sliceParam1,
+  sliceParam2,
+  passEncrypt,
+  letterCheck;
+
+// Assigning All variables value
+userInput = prompt().toLowerCase();
+letter = " abcdefghijklmnopqrstuvwxyz";
+number = "000102030405060708091011121314151617181920212223242526";
+passEncrypt = "";
+
+// Encryption loop starts here
+for (var x = 0; x < userInput.length; x++) {
+  letterCheck = userInput.charAt(x);
+  sliceParam1 = 0;
+  sliceParam2 = 2;
+  for (var i = 0; i < letter.length; i++) {
+    switch (letterCheck) {
+      case letter.charAt(i):
+        passEncrypt += number.slice(sliceParam1, sliceParam2);
+        break;
+    }
+    sliceParam1 += 2;
+    sliceParam2 += 2;
   }
 }
-document.write(
-  '<div class ="message"> Your password Encrypt is </div>' +
-    '<div class ="letterBox">' +
-    letterCollect +
-    "</div>"
-);
+
+document.write("<div>" + passEncrypt + "</div>");
+
+/*
+  ===<>===<>===<>===<>===<>===<>===
+  ===<>===Decryption Engine===<>===
+  ===<>===<>===<>===<>===<>===<>===
+*/
+
+// Declare All variables
+var passDecrypt, numberSlicePrm1, numberSlicePrm2;
+
+// Assigning All variables value
+passDecrypt = "";
+sliceParam1 = 0;
+sliceParam2 = 2;
+//-> letter = " abcdefghijklmnopqrstuvwxyz";
+//-> number = "000102030405060708091011121314151617181920212223242526";
+
+// Decryption loop starts here
+for (var x = 0; x < passEncrypt.length; x += 2) {
+  letterCheck = passEncrypt.slice(sliceParam1, sliceParam2);
+  numberSlicePrm1 = 0;
+  numberSlicePrm2 = 2;
+  for (var i = 0; i < letter.length; i++) {
+    switch (letterCheck) {
+      case number.slice(numberSlicePrm1, numberSlicePrm2):
+        passDecrypt += letter.charAt(i);
+    }
+    numberSlicePrm1 += 2;
+    numberSlicePrm2 += 2;
+  }
+  sliceParam1 += 2;
+  sliceParam2 += 2;
+}
+
+document.write("<div>" + passDecrypt + "</div>");
